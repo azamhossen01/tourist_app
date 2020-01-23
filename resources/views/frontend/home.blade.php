dd80b065aa94b523a7d2e16ec4abc62c10252f75
azamhossen01 <azamhossen01@gmail.com>
2020-01-23 23:37:20 +0600

    editing login and register form

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -14,28 +14,28 @@
             <div class="col-lg-4 col-md-6 banner-right">
                 <ul class="nav nav-tabs" id="myTab" role="tablist">
                   <li class="nav-item">
-                    <a class="nav-link active" id="flight-tab" data-toggle="tab" href="#flight" role="tab" aria-controls="flight" aria-selected="true">Flights</a>
+                    <a class="nav-link active" id="login-tab" data-toggle="tab" href="#flight" role="tab" aria-controls="flight" aria-selected="true">Login</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" id="hotel-tab" data-toggle="tab" href="#hotel" role="tab" aria-controls="hotel" aria-selected="false">Hotels</a>
+                    <a class="nav-link" id="registration-tab" data-toggle="tab" href="#hotel" role="tab" aria-controls="hotel" aria-selected="false">Registration</a>
                   </li>
-                  <li class="nav-item">
+                  {{-- <li class="nav-item">
                     <a class="nav-link" id="holiday-tab" data-toggle="tab" href="#holiday" role="tab" aria-controls="holiday" aria-selected="false">Holidays</a>
-                  </li>
+                  </li> --}}
                 </ul>
                 <div class="tab-content" id="myTabContent">
-                  <div class="tab-pane fade show active" id="flight" role="tabpanel" aria-labelledby="flight-tab">
+                  <div class="tab-pane fade show active" id="flight" role="tabpanel" aria-labelledby="login-tab">
                     <form class="form-wrap">
                         <input type="text" class="form-control" name="name" placeholder="From " onfocus="this.placeholder = ''" onblur="this.placeholder = 'From '">									
                         <input type="text" class="form-control" name="to" placeholder="To " onfocus="this.placeholder = ''" onblur="this.placeholder = 'To '">
-                        <input type="text" class="form-control date-picker" name="start" placeholder="Start " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Start '">
+                        {{-- <input type="text" class="form-control date-picker" name="start" placeholder="Start " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Start '">
                         <input type="text" class="form-control date-picker" name="return" placeholder="Return " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Return '">
                         <input type="number" min="1" max="20" class="form-control" name="adults" placeholder="Adults " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Adults '">
-                        <input type="number" min="1" max="20" class="form-control" name="child" placeholder="Child " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Child '">							
+                        <input type="number" min="1" max="20" class="form-control" name="child" placeholder="Child " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Child '">							 --}}
                         <a href="#" class="primary-btn text-uppercase">Search flights</a>									
                     </form>
                   </div>
-                  <div class="tab-pane fade" id="hotel" role="tabpanel" aria-labelledby="hotel-tab">
+                  <div class="tab-pane fade" id="hotel" role="tabpanel" aria-labelledby="registration-tab">
                     <form class="form-wrap">
                         <input type="text" class="form-control" name="name" placeholder="From " onfocus="this.placeholder = ''" onblur="this.placeholder = 'From '">									
                         <input type="text" class="form-control" name="to" placeholder="To " onfocus="this.placeholder = ''" onblur="this.placeholder = 'To '">
@@ -46,7 +46,7 @@
                         <a href="#" class="primary-btn text-uppercase">Search Hotels</a>									
                     </form>							  	
                   </div>
-                  <div class="tab-pane fade" id="holiday" role="tabpanel" aria-labelledby="holiday-tab">
+                  {{-- <div class="tab-pane fade" id="holiday" role="tabpanel" aria-labelledby="holiday-tab">
                     <form class="form-wrap">
                         <input type="text" class="form-control" name="name" placeholder="From " onfocus="this.placeholder = ''" onblur="this.placeholder = 'From '">									
                         <input type="text" class="form-control" name="to" placeholder="To " onfocus="this.placeholder = ''" onblur="this.placeholder = 'To '">
@@ -56,7 +56,7 @@
                         <input type="number" min="1" max="20" class="form-control" name="child" placeholder="Child " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Child '">							
                         <a href="#" class="primary-btn text-uppercase">Search Holidays</a>									
                     </form>							  	
-                  </div>
+                  </div> --}}
                 </div>
             </div>
         </div>
